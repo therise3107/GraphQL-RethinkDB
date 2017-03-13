53c25ec85010534d05e8febbb1bb7504fae45e4b
therise3107 <lyadav3107@gmail.com>
2017-03-13 18:13:41 +0530

    refactored server.js

--- server.js
+++ server.js
@@ -3,14 +3,11 @@ const app = express()
 const bodyParser = require('body-parser')
 const expressGraphQL = require('express-graphql')
 const schema = require('./schema/schema')
-const r = require('rethinkdb')
 
 app.use('/graphql', expressGraphQL({
   schema,
   graphiql: true
 }))
-
-app.use(bodyParser.json())
 
 app.listen(4000, _=> {
   console.log('listening on localhost:4000')
